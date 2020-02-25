e558121b51907c62600613704315969164ec6d27
rhlaroya <rhlaroya@gmail.com>
2020-02-24 20:22:21 -0500

    Pushing empty models

---
+++ campus/src/main/java/com/rms/model/AmenityType.java
@@ -0,0 +1,8 @@
+package com.rms.model;
+
+public class AmenityType {
+    private int id;
+    private String name;
+    private String abbrName;
+    
+}
---
+++ campus/src/main/java/com/rms/model/Campus.java
@@ -0,0 +1,5 @@
+package com.rms.model;
+
+public class Campus{
+    
+}
