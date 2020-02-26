65a39ca715dfda92c68f473299645b128f316adc
Sbdsmall <sbdsmall37@gmail.com>
2020-02-26 13:45:07 -0500

    pushing Controller annotations up

--- campus/src/main/java/com/rms/controller/AmenityStatusController.java
+++ campus/src/main/java/com/rms/controller/AmenityStatusController.java
@@ -1,5 +1,15 @@
 package com.rms.controller;
 
+import com.rms.dao.AmenityStatusDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AmenityStatusController {
+    
+    @Autowired
+    AmenityStatusDao asd;
+
     
 }
