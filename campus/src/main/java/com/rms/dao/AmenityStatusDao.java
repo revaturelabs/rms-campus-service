42fae145bb18782f7da4ecadeb35261db907a66f
maresh315 <mareshescoffery@gmail.com>
2020-02-27 11:46:53 -0500

    deleted the rest of amenity status and type, controller/service/dao

--- campus/src/main/java/com/rms/dao/AmenityStatusDao.java
+++
@@ -1,9 +0,0 @@
-package com.rms.dao;
-
-import com.rms.model.AmenityStatus;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AmenityStatusDao extends CrudRepository<AmenityStatus, Integer>{
-    
-}
