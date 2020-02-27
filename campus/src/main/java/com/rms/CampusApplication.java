af45a232d3310cb186f10b1c1997fba042048a64
Sbdsmall <sbdsmall37@gmail.com>
2020-02-27 13:43:45 -0500

    application.java changes and campusdb stuff

--- campus/src/main/java/com/rms/CampusApplication.java
+++ campus/src/main/java/com/rms/CampusApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
-@EnableEurekaClient
+// @EnableEurekaClient
 @SpringBootApplication
 public class CampusApplication {
 
