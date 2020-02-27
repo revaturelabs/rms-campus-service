591d511a860ff8689841e22ca16ea7b1d95a2957
maresh315 <mareshescoffery@gmail.com>
2020-02-27 11:44:35 -0500

    finished RoomController and AmenityController

--- campus/src/main/java/com/rms/controller/RoomController.java
+++ campus/src/main/java/com/rms/controller/RoomController.java
@@ -2,10 +2,14 @@ package com.rms.controller;
 
 import com.rms.dao.RoomDao;
 import com.rms.model.Room;
+import com.rms.service.RoomService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,15 +17,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoomController {
     
     @Autowired
-    RoomDao rd;
+    RoomDao rs;
 
-    @GetMapping("/id")
+    @GetMapping("/{id}")
     public Room findById(@PathVariable("id") int id){
-        return rd.findById(id).get();
+        return rs.findById(id).get();
+    }
+    
+    @PostMapping("/all")
+    public Iterable<Room> getAllRooms() {
+        return rs.findAll();
     }
 
+    @PostMapping
     public String insert(@RequestBody Room r){
-        rd.save(r);
-        return "Room has been added or updated";
+        rs.save(r);
+        return "Room has been added";
     }
+
+    @PutMapping("/updated")
+    public Room update(@RequestBody Room r){
+        return rs.save(r);
+    }
+
+    @DeleteMapping("/deleted")
+    public String delete(@RequestBody Room r) {
+        rs.delete(r);
+        return "Room has been deleted";
+    }
+
 }
