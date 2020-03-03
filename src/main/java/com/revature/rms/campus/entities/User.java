93d6837b606c783e9a867e459fdae568ddaf2ebd
Evan Hsi <evanhsi@Evans-MacBook-Pro.local>
2020-03-03 10:11:08 -0500

    Added mongo repository, set up package structure, added a sample test file for a user and its' repository.

---
+++ src/main/java/com/revature/rms/campus/entities/User.java
@@ -0,0 +1,21 @@
+package com.revature.rms.campus.entities;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
