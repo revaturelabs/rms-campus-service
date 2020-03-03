9de15c6ba84b5c7924bc3c4605db44e8b1c8518d
Niles Diggs <ndiggs@wesleyan.edu>
2020-03-03 14:07:42 -0500

    making service package with basic set up

--- src/main/java/com/revature/rms/campus/services/CampusService.java
+++ src/main/java/com/revature/rms/campus/services/CampusService.java
@@ -1,8 +1,19 @@
 package com.revature.rms.campus.services;
 
+import com.revature.rms.campus.repositories.CampusMongoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CampusService {
 
+    public CampusMongoRepository cMongoRepo;
+
+    @Autowired
+    public CampusService (CampusMongoRepository repo) {
+        super();
+        this.cMongoRepo = repo;
+    }
+
+    
 }
