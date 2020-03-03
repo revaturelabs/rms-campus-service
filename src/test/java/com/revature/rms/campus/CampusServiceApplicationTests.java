d551738427a1e9b13f12fc9f7fda1292ebfcba31
wsingleton <wezley.singleton@gmail.com>
2020-03-02 20:57:46 -0500

    init repo and project

---
+++ src/test/java/com/revature/rms/campus/CampusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.rms.campus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
