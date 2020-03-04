6ebe78979561c00040aa19784a5e5d642129cb5c
unknown <jaredallencarterjac@gmail.com>
2020-03-04 11:48:27 -0500

    building service delete matches Ivannas now, deleted my commented out code

--- src/main/java/com/revature/rms/campus/services/BuildingService.java
+++ src/main/java/com/revature/rms/campus/services/BuildingService.java
@@ -5,7 +5,7 @@ import com.revature.rms.campus.repositories.BuildingMongoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -14,12 +14,6 @@ public class BuildingService {
     @Autowired
     private BuildingMongoRepository buildingMongoRepository;
 
-//    public Building create(String name, String abbrName,
-//                           Address physicalAddress, Integer trainingLead, ArrayList<Amenity> amenities,
-//                           ArrayList<Room> rooms, ResourceMetadata resourceMetaData) {
-//        return buildingMongoRepository.save(new Building(name, abbrName, physicalAddress,
-//                trainingLead, amenities, rooms, resourceMetaData));
-//    }
 
     public Building save(Building building) {
         return buildingMongoRepository.save(building);
@@ -37,19 +31,7 @@ public class BuildingService {
         return buildingMongoRepository.findByName(name);
     }
 
-//    public Building update(String name, String abbrName,
-//                           Address physicalAddress, Integer trainingLead, ArrayList<Amenity> amenities,
-//                           ArrayList<Room> rooms, ResourceMetadata resourceMetaData) {
-//        Building building = buildingMongoRepository.findByName(name);
-//        building.setName(name);
-//        building.setAbbrName(abbrName);
-//        building.setPhysicalAddress(physicalAddress);
-//        building.setTrainingLead(trainingLead);
-//        building.setAmenities(amenities);
-//        building.setRooms(rooms);
-//        building.setResourceMetaData(resourceMetaData);
-//        return buildingMongoRepository.save(building);
-//    }
+
 
     public Building update(Building building) {
         return buildingMongoRepository.save(building);
