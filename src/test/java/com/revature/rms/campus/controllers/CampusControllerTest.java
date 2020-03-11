ca2a90543407e82388679b6c31c16302985b5f3f
Niles Diggs <ndiggs@wesleyan.edu>
2020-03-11 15:01:36 -0400

    controller unit tests

--- src/test/java/com/revature/rms/campus/controllers/CampusControllerTest.java
+++ src/test/java/com/revature/rms/campus/controllers/CampusControllerTest.java
@@ -4,6 +4,7 @@ import com.revature.rms.campus.entities.Address;
 import com.revature.rms.campus.entities.Building;
 import com.revature.rms.campus.entities.Campus;
 import com.revature.rms.campus.entities.ResourceMetadata;
+import com.revature.rms.campus.exceptions.InvalidInputException;
 import com.revature.rms.campus.exceptions.ResourceNotFoundException;
 import com.revature.rms.campus.services.CampusService;
 import org.junit.Test;
@@ -15,9 +16,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class CampusControllerTest {
@@ -73,29 +75,61 @@ public class CampusControllerTest {
         when(campusService.save(Mockito.any())).thenReturn(expectedResult);
         assertEquals(campusController.saveCampus(null), expectedResult);
     }
+
+    @Test
+    public void testGetCampusByValidId() {
+        String id = "32";
+        Campus expectedResult = new Campus("32", "University of South Florida", "USF", new Address(),
+                2, 3, 4, new Building[1], new int[3], new ResourceMetadata());
+
+        when(campusService.findById(Mockito.any())).thenReturn(Optional.of(expectedResult));
+        assertEquals(campusController.getCampusById(id),  expectedResult);
+    }
+
+    @Test (expected = ResourceNotFoundException.class)
+    public void testGetCampusWithByIdNotFound() {
+        String id = "21";
+        when(campusService.findById(Mockito.any())).thenReturn(Optional.empty());
+        campusController.getCampusById(id);
+    }
+
+    @Test(expected = InvalidInputException.class)
+    public void testGetCampusWithInvalidCampus() {
+        String id = "";
+        when(campusService.findById(Mockito.any())).thenReturn(null);
+        assertEquals(campusController.getCampusById(id), null);
+    }
+
+    @Test
+    public void testUpdateCampusWithValidCampus() {
+        Campus testCampus = new Campus("University of South Florida", "USF", new Address(),
+                2, 3, 4, new Building[1], new int[3], new ResourceMetadata());
+
+        Campus expectedResult = new Campus("32", "University of South Florida", "USF", new Address(),
+                2, 3, 4, new Building[1], new int[3], new ResourceMetadata());
+
+        when(campusService.update(Mockito.any())).thenReturn(expectedResult);
+        assertEquals(campusController.updateCampus(testCampus), expectedResult);
+    }
+
+    @Test
+    public void testDeleteCampusWithValidId() {
+        Campus testCampus = new Campus("32","University of South Florida", "USF", new Address(),
+                2, 3, 4, new Building[1], new int[3], new ResourceMetadata());
+
+        when(campusService.findById(Mockito.any())).thenReturn(Optional.of(testCampus));
+        campusController.deleteCampusById(testCampus.getId());
+        verify(campusService, times(1)).delete(testCampus.getId());
+    }
+
+    @Test(expected = InvalidInputException.class)
+    public void testDeleteCampusWithInvalidId() {
+        Campus testCampus = new Campus("32","University of South Florida", "USF", new Address(),
+                2, 3, 4, new Building[1], new int[3], new ResourceMetadata());
+
+        String testId = "-1";
+        when(campusService.findById(Mockito.any())).thenReturn(Optional.of(testCampus));
+        campusController.deleteCampusById(testId);
+        verify(campusService, times(0)).delete(testId);
+    }
 }
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//@RunWith(MockitoJUnitRunner.class)
-//
-//public class CampusControllerTest {
-//
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    @InjectMocks
-//    private CampusService campusService;
-//
-//    @Test
-//    public void testCampusWithCampusWillReturnJSONArray() throws Exception {
-//        Campus testCampus = new Campus("32", "University of South Florida", "USF", new Address(),
-//                2, 3, 4, new Building[1], new int[3], new ResourceMetadata());
-//
-//        List<Campus> testListCampus = Arrays.asList(testCampus);
-//        when(campusService.findAll()).thenReturn(testListCampus);
-//
-//        mvc.perform(MockMvcRequestBuilders.get("/v2/campus").contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(MockMvcResultMatchers.status().isOk());
-//    }
-//
-//}
