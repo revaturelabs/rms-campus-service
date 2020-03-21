b73f2e9ba7842e39ac8085beb9fe43d5876e5988
Wezley <wezley.singleton@gmail.com>
2020-03-20 21:24:33 -0400

    corrected room endpoint to be plural

--- src/main/java/com/revature/rms/campus/controllers/RoomController.java
+++ src/main/java/com/revature/rms/campus/controllers/RoomController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/room")
+@RequestMapping("/rooms")
 public class RoomController extends ResourceController<Room> {
 
     public RoomController(RoomService service) {
