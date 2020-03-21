e4ffe42fc90135ac72052f1b2062686621fa49fc
Wezley <wezley.singleton@gmail.com>
2020-03-20 21:25:27 -0400

    impl amenity model and status enum

--- src/main/java/com/revature/rms/campus/entities/Amenity.java
+++ src/main/java/com/revature/rms/campus/entities/Amenity.java
@@ -1,4 +1,17 @@
 package com.revature.rms.campus.entities;
 
 public class Amenity {
+
+    private String type;
+    private AmenityStatus status;
+
+    public Amenity() {
+        super();
+    }
+
+    public Amenity(String type, AmenityStatus status) {
+        this.type = type;
+        this.status = status;
+    }
+
 }
--- src/main/java/com/revature/rms/campus/entities/AmenityStatus.java
+++ src/main/java/com/revature/rms/campus/entities/AmenityStatus.java
@@ -2,5 +2,19 @@ package com.revature.rms.campus.entities;
 
 public enum AmenityStatus {
 
+    OK("Ok - Stocked for 2+ weeks"),
+    LOW("Low - Less than 2 week supply left"),
+    OUT("Out - Restock ASAP");
+
+    private String description;
+
+    AmenityStatus(String desc) {
+        this.description = desc;
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
 
 }
