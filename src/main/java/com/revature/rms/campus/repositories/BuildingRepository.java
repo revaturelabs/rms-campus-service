7b18a98f565c1605cd961d5a278ccfe8ce9bd376
Wagenheim <wagenheimk@me.com>
2020-06-19 15:07:53 -0400

    removed mongo comments

--- src/main/java/com/revature/rms/campus/repositories/BuildingRepository.java
+++ src/main/java/com/revature/rms/campus/repositories/BuildingRepository.java
@@ -2,20 +2,13 @@ package com.revature.rms.campus.repositories;
 
 import com.revature.rms.campus.entities.Building;
 //import org.springframework.data.mongodb.repository.MongoRepository;
+import com.revature.rms.campus.entities.ResourceMetadata;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 import java.util.List;
 import java.util.Optional;
-
-//public interface BuildingMongoRepository extends MongoRepository<Building, String> {
-//    public Building findByName(String name);
-//
-//    public Building findByTrainingLead(Integer id);
-//
-//
-//}
 
 @Repository
 public interface BuildingRepository extends CrudRepository<Building, Integer>{
