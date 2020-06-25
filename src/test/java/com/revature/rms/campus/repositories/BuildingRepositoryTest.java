7cc0ad7887bcba30d1af27a2ab892021e6de82ce
jdvalencia <jdvalencia@users.noreply.github.com>
2020-06-24 23:53:13 -0400

    Added successful testing to the repos

--- src/test/java/com/revature/rms/campus/repositories/BuildingRepositoryTest.java
+++ src/test/java/com/revature/rms/campus/repositories/BuildingRepositoryTest.java
@@ -1,8 +1,9 @@
 package com.revature.rms.campus.repositories;
 
-import com.revature.rms.campus.entities.Address;
-import com.revature.rms.campus.entities.Building;
-import org.junit.jupiter.api.*;
+import com.revature.rms.campus.entities.*;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -11,11 +12,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+//import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -36,7 +40,7 @@ public class BuildingRepositoryTest {
     public void tearDown() throws Exception {
         this.sut.deleteAll();
     }
-    @org.junit.Test
+    @Test
     public void testSaveBuilding() {
         Building building = new Building("Test", "TEST", new Address(1,"","","","",""), 10, null, null, null);
         sut.save(building);
@@ -46,34 +50,39 @@ public class BuildingRepositoryTest {
 
         assertThat(result.getAbbrName()).isEqualTo("TEST");
     }
-//
-//    @org.junit.Test
-//    public void testFindAll() {
-//        //Iterable<Building> buildingList = sut.findAll();
-//        Building building = new Building("Test1", "TEST1", new Address(10,"asd","asd","asd","asd","asd"), 12, null, null, null);
-//        Building building2 = new Building("Test2", "TEST2", new Address(11,"asdf","asdf","asdf","asdf","asdf"), 13, null, null, null);
-//        sut.save(building);
-//        sut.save(building2);
-//        int originalCount = 0;
-//        int testCount = 0;
-//        Iterable<Building> buildingList2 = sut.findAll();
-////        for (Building b : buildingList) {
-////            originalCount++;
-////        }
-//        for (Building b : buildingList2) {
-//            testCount++;
-//        }
-//        assertThat(originalCount).isEqualTo(testCount+2);
-//
-//    }
-//    @Test
-//    public void testFindByNameAndFindById() {
-//        Building building1 = sut.findByName("NEC");
-//        assertNotNull(building1);
-//        Building building2 = sut.findById(building1.getId()).get();
-//        assertNotNull(building2);
-//        assertEquals( building1, building2 );
-//    }
+
+    @Test
+    public void testFindAll() {
+        Building building = new Building("Test1", "TEST1", new Address(1,"asd","asd","asd","asd","asd"), 12, null, null, null);
+        Building building2 = new Building("Test2", "TEST2", new Address(1,"asdf","asdf","asdf","asdf","asdf"), 13, null, null, null);
+        sut.save(building);
+        sut.save(building2);
+        int originalCount = 0;
+        int testCount = 0;
+        Iterable<Building> buildingList2 = sut.findAll();
+        List<Building> result = new ArrayList<>();
+        for (Building b : buildingList2) {
+            testCount++;
+            result.add(b);
+        }
+        assertThat(4).isEqualTo(result.size());
+
+    }
+    @Test
+    public void testFindByName() {
+        Amenity amenity = new Amenity(1,AmenityType.COFFEE, AmenityStatus.OK);
+        ResourceMetadata rmd = new ResourceMetadata(1,1,"",1,"",1,true);
+        List<Amenity> amenities = new ArrayList<>();
+        amenities.add(amenity);
+        Room room = new Room(1,"200",10,new ArrayList<>(),1,new ArrayList<>(),rmd);
+        List<Room> rooms = new ArrayList<>();
+        rooms.add(room);
+        Building building = new Building("Test3", "TEST3", new Address(1,"asd","asd","asd","asd","asd"), 12, amenities, rooms, rmd);
+        sut.save(building);
+        Building building1 = sut.findByName("Test3");
+        assertNotNull(building1);
+        assertEquals( building1.getAbbrName(), building.getAbbrName() );
+    }
 //    @Test
 //    public void testUpdate() {
 //        Building building1 = sut.findByName("NEC");
--- src/test/java/com/revature/rms/campus/repositories/CampusRepositoryTest.java
+++ src/test/java/com/revature/rms/campus/repositories/CampusRepositoryTest.java
@@ -1,4 +1,70 @@
-//package com.revature.rms.campus.repositories;
-//
-//public class CampusMongoRepositoryTest {
-//}
+package com.revature.rms.campus.repositories;
+
+import com.revature.rms.campus.entities.Address;
+import com.revature.rms.campus.entities.Campus;
+import com.revature.rms.campus.entities.ResourceMetadata;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CampusRepositoryTest {
+
+    @Autowired
+    private CampusRepository sut;
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        this.sut.deleteAll();
+    }
+
+    @Test
+    public void testFindByName() {
+        Campus campus = new Campus("University of Central Florida", "UCF",
+                new Address(1,"","","","",""),1,1,1,
+                new ArrayList<>(),new ArrayList<>(),
+                new ResourceMetadata(1,1,"",1,"",1,true));
+        sut.save(campus);
+        Campus campus1 = sut.findByName("University of Central Florida");
+        assertThat(campus1.getAbbrName()).isEqualTo(campus.getAbbrName());
+    }
+
+    @Test
+    public void testFindByStagingManagerId() {
+        Campus campus = new Campus("University", "USFT",
+                new Address(1,"","","","",""),1,7,1,
+                new ArrayList<>(),new ArrayList<>(),
+                new ResourceMetadata(1,1,"",1,"",1,true));
+
+        sut.save(campus);
+        List<Campus> campusList = sut.findByStagingManagerId(7);
+
+        assertThat(campusList.size()).isEqualTo(1);
+
+    }
+
+    @Test
+    public void testFindByTrainingManagerId() {
+        Campus campus = new Campus("University of Test", "USFTEST",
+                new Address(1,"","","","",""),4,7,1,
+                new ArrayList<>(),new ArrayList<>(),
+                new ResourceMetadata(1,1,"",1,"",1,true));
+
+        sut.save(campus);
+        List<Campus> campusList = sut.findByTrainingManagerId(4);
+
+        assertThat(campusList.size()).isEqualTo(1);
+
+    }
+}
