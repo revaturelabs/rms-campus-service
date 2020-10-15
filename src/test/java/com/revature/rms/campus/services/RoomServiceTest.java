66f174723691f6a4d08558110cbf6d868f027cb8
mcoffma1 <47868221+mcoffma1@users.noreply.github.com>
2020-10-14 21:16:29 -0400

    Rewriting tests after core refactor - 53%

--- src/test/java/com/revature/rms/campus/services/RoomServiceTest.java
+++ src/test/java/com/revature/rms/campus/services/RoomServiceTest.java
@@ -1,5 +1,6 @@
 package com.revature.rms.campus.services;
 
+import com.revature.rms.campus.entities.Building;
 import com.revature.rms.campus.entities.Room;
 import com.revature.rms.campus.entities.RoomStatus;
 import com.revature.rms.campus.exceptions.InvalidInputException;
@@ -7,7 +8,8 @@ import com.revature.rms.campus.exceptions.ResourceNotFoundException;
 import com.revature.rms.campus.repositories.RoomRepository;
 import com.revature.rms.campus.repositories.RoomStatusRepository;
 import com.revature.rms.core.metadata.*;
-import org.junit.Ignore;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -27,273 +29,138 @@ public class RoomServiceTest {
 
     @Mock
     RoomRepository repo;
+
     @Mock
     RoomStatusRepository roomStatusRepository;
 
     @InjectMocks
     RoomService sut;
 
-    /**
-     * testSaveWithValidRoom() ensures RoonService.save() is functioning properly with the valid or correct input.
-     * A non-null room object should be returned.
-     */
-    @Test
-    public void testSaveWithValidRoom(){
-        Room testRoom = new Room("2301", 25,  new ArrayList<RoomStatus>(5),
-                1612, new ArrayList<Integer>(3), new ResourceMetadata( 1,"3.16.2020 10:00 PM", 1, "3.16.2020 10:00 PM", 1, true));
+    //Spies
+    RoomService spyService = new RoomService();
+    RoomStatus spyStatus = new RoomStatus();
 
-        Room expectedResult = new Room(1,"2301", 25,  new ArrayList<RoomStatus>(5),
-                1612, new ArrayList<Integer>(3), new ResourceMetadata( 1,"3.16.2020 10:00 PM", 1, "3.16.2020 10:00 PM", 1, true));
+    //Test Data
+    List<Room> testRooms;
+    Room nullRoom;
+    ArrayList<Integer> workorders;
+    ArrayList<RoomStatus> testStatuses;
 
-        when(repo.save(Mockito.any())).thenReturn(expectedResult);
+    @Before
+    public void setup() {
+        //Room statuses
+        RoomStatus s1 = new RoomStatus(1, false, false, "Jan 28, 2020", 1, "And So It Begins...");
+        RoomStatus s2 = new RoomStatus(2, true, false, "Jan 29, 2020", 1, "And Where it Is...");
+        RoomStatus s3 = new RoomStatus(3, false, true, "Jan 30, 2020", 1, "And How It Ends...");
+        testStatuses = new ArrayList<>();
+        testStatuses.add(s1);
+        testStatuses.add(s2);
+        testStatuses.add(s3);
 
-        Room actualResults = sut.save(testRoom);
+        //Work Orders
+        workorders = new ArrayList<>();
+        workorders.add(1);
+        workorders.add(2);
+        workorders.add(3);
 
-        assertEquals(actualResults, expectedResult);
+        //Rooms
+        Room r1 = new Room(1, "404", 1, testStatuses, 1, workorders, new ResourceMetadata());
+        Room r2 = new Room(2, "606", 25, testStatuses, 2, workorders, new ResourceMetadata());
+        Room r3 = new Room(3, "808", 300, testStatuses, 3, workorders, new ResourceMetadata());
+        testRooms = new ArrayList<>();
+        testRooms.add(r1);
+        testRooms.add(r2);
+        testRooms.add(r3);
+
+        //Spies
+        spyService = spy(spyService);
+        spyStatus = spy(spyStatus);
+
     }
 
-    /**
-     * testSaveWithNullRoom() ensures RoomService.save() will not work if a null room object is being saved. If the room
-     * object is null, a ResourceNotFoundException will be thrown since the method does not meet the desired criteria
-     * for the method to execute properly.
-     */
+    @After
+    public void tearDown() {
+        testStatuses = null;
+        workorders = null;
+        testRooms = null;
+    }
+
+    @Test
+    public void testSaveRoom() {
+        when(repo.save(testRooms.get(0))).thenReturn(testRooms.get(0));
+        assertEquals(testRooms.get(0), sut.save(testRooms.get(0)));
+    }
+
+//    @Test
+//    public void testSaveRoomSpy() {
+//        when(repo.save(testRooms.get(0))).thenReturn(testRooms.get(0));
+//    }
+
     @Test(expected = ResourceNotFoundException.class)
-    public void testSaveWithNullRoom(){
-        //Arrange
-        Room testRoom = new Room("2301", 25,  new ArrayList<RoomStatus>(5),
-                1612, new ArrayList<Integer>(3), new ResourceMetadata());
-
-        Room expectedResult = new Room(1,"2301", 25,  new ArrayList<RoomStatus>(5),
-                1612, new ArrayList<Integer>(3), new ResourceMetadata());
-        //Act
-        Room actualResult = sut.save(null);
-
-        //Assert
-        assertEquals(actualResult, expectedResult);
+    public void testSaveRoomNullRoom() {
+        sut.save(nullRoom);
     }
 
-    /**
-     * testFindAll() ensures RoomService().findAll() returns a list of all the existing Room objects.
-     */
     @Test
-    public void testFindAll(){
-        Room testRoom = new Room("2301", 25,  new ArrayList<RoomStatus>(5),
-                1612, new ArrayList<Integer>(3), new ResourceMetadata());
-
-        List<Room> mockRoomList = new ArrayList<>();
-        when(repo.findAll()).thenReturn(mockRoomList);
-        assertEquals(mockRoomList, sut.findAll());
+    public void testGetAllRooms() {
+        when(repo.findAll()).thenReturn(testRooms);
+        assertEquals(testRooms, sut.findAll());
     }
 
-    /**
-     * testFindAllRoomWithNoRoom() returns an empty list rather than returning null, thus avoiding a potential
-     * NullPointerException in the future.
-     */
     @Test
-    public void testFindAllRoomWithNoRoom(){
-        List<Room> mockRoomList = Collections.emptyList();
-        when(repo.findAll()).thenReturn(mockRoomList);
-        assertEquals(mockRoomList, sut.findAll());
+    public void testGetById() {
+        when(repo.findById(testRooms.get(0).getId())).thenReturn(Optional.ofNullable(testRooms.get(0)));
+        assertEquals(Optional.ofNullable(testRooms.get(0)), sut.findById(testRooms.get(0).getId()));
     }
 
-    /**
-     * testFindByRoomNumberWithValidRoomNumber() returns the object containing the same room number as the one provided.
-     */
-    @Test
-    public void testFindByRoomNumberWithValidRoomNumber(){
-        Optional<Room> expectedResult = Optional.of(new Room(23,"2304", 25,  new ArrayList<RoomStatus>(5),
-                1612, new ArrayList<Integer>(3), new ResourceMetadata()));
-
-        when(repo.findByRoomNumber(Mockito.any())).thenReturn(expectedResult);
-        Optional<Room> actualResult = sut.findByRoomNumber("2304");
-        assertEquals(actualResult, expectedResult);
-    }
-
-    /**
-     * testFindByInvalidRoomNumber ensures that findByRoomNumber throws an
-     * InvalidInputException when an invalid ID is entered.
-     */
-    @Test (expected = InvalidInputException.class)
-    public void testFindByInvalidRoomNumber() {
-        Optional<Room> expectedResult = Optional.of(new Room(23,"2304", 25,  new ArrayList<RoomStatus>(5),
-                1612, new ArrayList<Integer>(3), new ResourceMetadata()));
-
-        Optional<Room> actualResult = sut.findByRoomNumber("-100");
-    }
-
-    /**
-     * findByRoomNumberWithValidRoomNumberNotFound() throws a ResourceNotFoundException when provided a room number
-     * that is a correct value but is not associated with room numbers.
-     */
-    @Test(expected = ResourceNotFoundException.class)
-    public void findByRoomNumberWithValidRoomNumberNotFound(){
-        when(repo.findByRoomNumber(Mockito.any())).thenReturn(Optional.empty());
-        sut.findByRoomNumber("5");
-    }
-
-    /**
-     * testFindByResourceOwner returns a list of Room objects where the resourceOwner's ID
-     * matches the int specified in the parameter of findByResourceOwner.
-     */
-    @Test
-    public void testFindByResourceOwner() {
-        List<Room> rooms = new ArrayList<>();
-        rooms.add(new Room(23,"2304", 25,  new ArrayList<RoomStatus>(5),
-                1612, new ArrayList<Integer>(3), new ResourceMetadata()));
-
-        when(repo.findAll()).thenReturn(rooms);
-        assertEquals(rooms, sut.findByResourceOwner(1));
-
-    }
-
-    /**
-     * testFindByResourceOwnerInvalidId returns an InvalidInputException when
-     * the ID is invalid.
-     */
-    @Test (expected = InvalidInputException.class)
-    public void testFindByResourceOwnerInvalidId() {
-        sut.findByResourceOwner(-10);
-    }
-
-    /**
-     * testFindByIdWithValidId() ensures RoomService.findById() returns the object containing the same id as the one provided.
-     */
-    @Test
-    public void testFindByIdWithValidId(){
-        Optional<Room> expectedResult = Optional.of(new Room(23,"2304", 25,  new ArrayList<RoomStatus>(5),
-                1612, new ArrayList<Integer>(3), new ResourceMetadata()));
-
-        when(repo.findById(Mockito.any())).thenReturn(expectedResult);
-        Optional<Room> actualResult = sut.findById(23);
-        assertEquals(actualResult, expectedResult);
-    }
-
-    /**
-     * testFindByIdWithValidIdNotFound() throws a ResourceNotFoundException when provided a correct id but,
-     * there is no room associated with it.
-     */
-    @Test(expected = ResourceNotFoundException.class)
-    public void testFindByIdWithValidIdNotFound(){
-     when(repo.findById(Mockito.any())).thenReturn(Optional.empty());
-     sut.findById(91);
-    }
-
-    /**
-     * findByIdWithInvalidId() throws an InvalidInputException when the provided id is less than or equal to zero or
-     * empty, that is the provided field is empty.
-     */
     @Test(expected = InvalidInputException.class)
-    public void findByIdWithInvalidId(){
-        sut.findById(0); sut.findById(0);
-        verify(repo.findById(0), times(0));
+    public void testGetByIdFailed() {
+        int id = 0;
+        sut.findById(id);
     }
 
-    /**
-     * testFindMaxOccupancyRooms() returns a list of room objects whose occupancy is equal to the provided number of occupancy.
-     */
+    @Test(expected = ResourceNotFoundException.class)
+    public void testGetByIdNotFound() {
+        when(sut.findById(99999)).thenThrow(ResourceNotFoundException.class);
+    }
+
     @Test
-    public void testFindMaxOccupancyRooms(){
-        Room testRoom = new Room("2301", 25,  new ArrayList<RoomStatus>(5),
-                1612, new ArrayList<Integer>(3), new ResourceMetadata());
-
-        List<Room> occupancyRoomList = new ArrayList<>();
-        when(repo.findByMaxOccupancy(Mockito.anyInt())).thenReturn(occupancyRoomList);
-        assertEquals(occupancyRoomList, sut.findByMaxOccupancy(35));
+    public void testGetByRoomNumber() {
+        when(repo.findByRoomNumber(testRooms.get(0).getRoomNumber())).thenReturn(Optional.ofNullable(testRooms.get(0)));
+        assertEquals(Optional.ofNullable(testRooms.get(0)), sut.findByRoomNumber(testRooms.get(0).getRoomNumber()));
     }
 
-    /**
-     * testUpdateWithValidRoom() passes in a room object with changed fields to the roomMongoRepository.save() to
-     * persist the changes and return the updated saved room object. There is no need for a null room check since the
-     * object already exists.
-     */
-    @Test
-    public void testUpdateWithValidRoom(){
-        Room testRoom = new Room(1,"2301", 25,  new ArrayList<RoomStatus>(5),
-                1612, new ArrayList<Integer>(3), new ResourceMetadata());
-
-        Room expectedResult = new Room("2301", 25,  new ArrayList<RoomStatus>(5),
-                1612, new ArrayList<Integer>(3), new ResourceMetadata());
-
-        when(repo.findById(testRoom.getId())).thenReturn(Optional.of(expectedResult));
-        when(repo.save(Mockito.any())).thenReturn(expectedResult);
-
-        testRoom.setRoomNumber("2302"); // was 2301
-
-        assertEquals(sut.update(testRoom), expectedResult);
-    }
-
-    /**
-     * Soft Delete was implemented in RoomService. This was so we can archive data, and not permanently or hard delete data.
-     *
-     * testDeleteWithValidId() returns an updated room object. This method retrieves the specific room object byt its id,
-     * then it sets the active field to false. The changed room object is saved or updated and returned to us.
-     */
-    @Test
-    @Ignore
-    public void testDeleteWithValidId(){
-        Room testRoom = new Room(1,"2301", 25,  new ArrayList<RoomStatus>(5),
-                1612, new ArrayList<Integer>(3), new ResourceMetadata());
-
-        when(repo.findById(1)).thenReturn(Optional.of(testRoom));
-
-        assertEquals(testRoom, sut.delete(1));
-    }
-
-    /**
-     * testDeleteWithInvalidId() ensures that roomMongoRepository.deleteById() is not run when provided an invalid id.
-     * Instead it throws an InvalidInputException.
-     */
     @Test(expected = InvalidInputException.class)
-    public void testDeleteWithInvalidId(){
-        Room testRoom = new Room("2301", 25,  new ArrayList<RoomStatus>(5),
-                1612, new ArrayList<Integer>(3), new ResourceMetadata());
-
-        sut.delete(-1);
-        verify(repo, times(1)).deleteById(-1);
+    public void testGetByRoomNumberNoRoom() {
+        when(sut.findByRoomNumber("")).thenThrow(InvalidInputException.class);
+    }
+    
+    @Test(expected = InvalidInputException.class)
+    public void testGetByRoomNumberParseError() {
+        when(sut.findByRoomNumber("0")).thenThrow(InvalidInputException.class);
     }
 
-    /**
-     * testFindAllStatusBySubmitter ensures that findAllStatusBySubmitter returns a list of RoomStatus objects
-     */
+    @Test(expected = ResourceNotFoundException.class)
+    public void testGetByRoomNumberNotFound() {
+        when(sut.findByRoomNumber("999999")).thenThrow(ResourceNotFoundException.class);
+    }
+
     @Test
-    public void testFindAllStatusBySubmitter() {
-        List<RoomStatus> roomStatuses = new ArrayList<>();
-        when(roomStatusRepository.findAllBySubmitterId(1)).thenReturn(roomStatuses);
-        assertEquals(roomStatuses, sut.findAllStatusBySubmitter(1));
+    public void testGetByMaxOccupancy() {
+        when(repo.findByMaxOccupancy(testRooms.get(0).getMaxOccupancy())).thenReturn(Collections.singletonList(testRooms.get(0)));
+        assertEquals(Collections.singletonList(testRooms.get(0)), sut.findByMaxOccupancy(testRooms.get(0).getMaxOccupancy()));
     }
 
-    /**
-     * testFindAllStatusByDate ensures that findAllStatusByDate returns a list of RoomStatus objects
-     */
-    @Test
-    public void testFindAllStatusByDate() {
-        List<RoomStatus> roomStatuses = new ArrayList<>();
-        String date = "1/6/2020";
-        when(roomStatusRepository.findAllBySubmittedDateTime(date)).thenReturn(roomStatuses);
-        assertEquals(roomStatuses, sut.findAllStatusByDate(date));
-    }
+//    @Test
+//    public void testGetByResourceOwner() {
+//        when(repo.findAll()).thenReturn(testRooms);
+//        assertEquals(sut.findByResourceOwner(1), testRooms.get(0));
+//    }
 
-    /**
-     * testFindStatusById ensures that findStatusById returns an Optional of
-     * a RoomStatus object.
-     */
-    @Test
-    public void testFindStatusById() {
-
-        Optional<RoomStatus> _roomStatus = Optional.of(new RoomStatus());
-        when(roomStatusRepository.findById(1)).thenReturn(_roomStatus);
-        assertEquals(_roomStatus, sut.findStatusById(1));
-    }
-
-    /**
-     * testFindAllStatus ensures that findAllStatus returns a list of
-     * RoomStatus objects.
-     */
-    @Test
-    public void testFindAllStatus() {
-        List<RoomStatus> roomStatuses = new ArrayList<>();
-        when(roomStatusRepository.findAll()).thenReturn(roomStatuses);
-        assertEquals(roomStatuses, sut.findAllStatus());
-    }
+//    @Test
+//    public void testUpdateRoom() {
+//        when(repo.save(testRooms.get(0))).thenReturn(testRooms.get(1));
+//        assertEquals(testRooms.get(1), sut.update(testRooms.get(0)));
+//    }
 
 }
